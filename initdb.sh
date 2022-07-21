251a246616bb1222f1eed67b423c00bdd76a6159
natohutch <toadestra@gmail.com>
2022-07-21 17:56:27 +1000

    added basic blob download functionality

--- initdb.sh
+++ initdb.sh
@@ -4,4 +4,5 @@ psql -U postgres -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_raster; 
 psql -U postgres -c "CREATE SERVER floodaware_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '${PG_SERVER_HOST}', dbname '${PG_SERVER_DB}', port '5432')"
 psql -U postgres -c "CREATE USER MAPPING FOR postgres SERVER floodaware_server OPTIONS (user '${PG_SERVER_USER}', password '${PG_SERVER_PASS}')"
 psql -U postgres -c "CREATE FOREIGN TABLE raster_temp2 (rid integer, rast raster) SERVER floodaware_server"
+psql -U postgres -c "CREATE FOREIGN TABLE rainfall (stamp timestamp without time zone, val double precision, id integer) SERVER floodaware_server"
 exec $@
--- main.py
+++ main.py
@@ -2,39 +2,54 @@ from azure.storage.blob import BlobServiceClient
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
-import os, json
+import os, json, re
 
-print(BlobServiceClient)
+last_file = ""
 
 connstr = os.environ["AZURE_BLOB_CONNSTR"]
+blob_service_client = BlobServiceClient.from_connection_string(connstr)
+container_name = "rainfields3"
+blob_client = blob_service_client.get_container_client(container=container_name)
 
-engine = create_engine(f"postgresql://postgres@localhost:5432", future=True)
+local_path = "./tmp"
+os.mkdir(local_path)
+
+engine = create_engine(f"postgresql://postgres@postgres?host=/var/run/postgresql/", future=True)
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/7Wekkt9Hp2kI9LX4pZ0LzxRcebTdiReYI5VibwEv2OI3BoXSYYLiSJPCaD5X9UB1")
+@app.route("/7Wekkt9Hp2kI9LX4pZ0LzxRcebTdiReYI5VibwEv2OI3BoXSYYLiSJPCaD5X9UB1", methods=["POST"])
 def hook():
-    with engine.connect() as conn:
-        result = conn.execute(text("""
-        SELECT 1,2,3
-        """))
-        return(result.all()[0][0])
+    global last_file
+    global blob_client
+
+    json = request.get_json()
+    file_name = json["file-name"]
+
+    if (file_name != last_file):
+        blob_file_name = re.search("/blobs/(.*)$", file_name).group(1)
+        timestamp_text = re.search("RF3\.(.*)\.nc$", blob_file_name).group(1)
+        
+        with open("./tmp/current.nc", "wb") as download_file:
+          download_file.write(blob_client.download_blob(blob_file_name).readall())
+        
+        os.system("raster2pgsql tmp/current.nc temporary_raster | psql -U postgres")
+
+        with engine.connect() as conn:
+            result = conn.execute(text("""
+            SELECT * FROM temporary_raster
+            """))
+            results = result.all()
+            print(results)
+
+    last_file = file_name
+
+    print(file_name)
+    
+    return file_name
 
 if (__name__ == "__main__"):
     app.run(host="0.0.0.0", port=80)
 
-# container_name = "rainfields3"
-
-# blob_service_client = BlobServiceClient.from_connection_string(connstr)
-
-# # Create a local directory to hold blob data
-# local_path = "./tmp"
-
-# blob_file_name = "IDR03AR.RF3.20220718103000.nc"
-# blobl_file_path = "/blobServices/default/containers/rainfields3/blobs/"
-
-# blob_client = blob_service_client.get_container_client(container= container_name) 
-
-# with open("./tmp/"+blob_file_name, "wb") as download_file:
-#  download_file.write(blob_client.download_blob(blob_file_name).readall())
+#'/blobServices/default/containers/rainfields3/blobs/IDR03AR.RF3.20220721020500.nc'
